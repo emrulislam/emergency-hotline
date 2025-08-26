e7fbd39b4ea385b5ea262a572a294736e9906e9f
Emrul Islam <emrul.mpl@gmail.com>
2025-08-27 04:21:12 +0600

    added js functionality for heart counter and call buttons

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
-//  Heart Icons//
+// heart Icons
 
-// Clicking on the 💗 heart icon of any card will increase the count in the Navbar
+const heartCounter = document.getElementById("heart-counter");
+const heartButtons = document.getElementsByClassName("heart-button");
 
-//  Call Buttons//
+for (const heartBtn of heartButtons) {
+  heartBtn.addEventListener("click", function () {
+    let count = Number(heartCounter.innerText);
+    count++;
+    heartCounter.innerText = count;
+  });
+}
 
-// On clicking a card's Call Button, following actions will happen:
-    // Show an alert with a message including the service name and number
-    // Each call will cut 20 coins. Reduce Coin after each click.
-    // If coins are less than 20, show a relevant alert and terminate the process.
-    // Add this service into the Call History section with:
-        // Service name
-        // Service number
+//  Call Buttons
 
+const callButtons = document.getElementsByClassName("call-button");
+const coinCounter = document.getElementById("coin-counter");
+const singleCallBalance = 20;
+const historyContainer = document.getElementById("history-container")
+const clearButton = document.getElementById("clear-button")
 
-//  Call History Section//
+for (const callBtn of callButtons) {
+  callBtn.addEventListener("click", function (e) {
+    const serviceName =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+    const serviceNumber =
+      callBtn.parentNode.parentNode.children[1].children[2].innerText;
+    let coinCount = Number(coinCounter.innerText);
 
-// Show all called services with name & number. This will empty initially. when call button clicked it will filled dynamically.
-// A Clear History button on the right
-// Clicking this button will remove all data from call history
+    if (coinCount < singleCallBalance) {
+      alert("Insufficient coins! Need at least 20 coins to make a call.");
+      return;
+    }
+    alert(`a call to ${serviceName} in ${serviceNumber}`);
+    coinCount -= singleCallBalance;
+    coinCounter.innerText = coinCount;
 
+    const historyItems = document.createElement("div")
+    historyItems.classList.add("bg-[#FAFAFA]","p-2", "rounded-lg", "flex", "justify-between", "items-center","my-4")
+    historyItems.innerHTML=
+    `
+    <div>
+              <h1 class="text-black font-bold text-xl">${serviceName}</h1>
+              <p class="text-gray-400 text-xl">${serviceNumber}</p>
+            </div>
+            <div>
+              <p class="text-[#111111] text-xl">${new Date().toLocaleTimeString('en-US', { hour12: true })}</p>
+            </div>
 
+    `
+    historyContainer.appendChild(historyItems)
+
+    clearButton.addEventListener("click",function(e){
+        historyContainer.innerHTML=""
+    })
+  });
+}
+
+// Copy Button
 // Challenges Part (10 Marks)//
+// On clicking the Copy button, show an alert and increase the copy count (3 Marks)
+// Hotline number will be copied on click so it can be pasted anywhere (4 Marks)
+// id
+// copy-counter-button
+// class names
+//  copy-button
 
-    // On clicking the Copy button, show an alert and increase the copy count (3 Marks)
-
-    // Hotline number will be copied on click so it can be pasted anywhere (4 Marks)
-
-
-    // id
-
-    // heart-counter
-    // coin-counter
-    // copy-counter-button
-    // clear-button
-    
-    
-    // class names
-
-
-    // heart-button
-    //  service-name
-    //  service-number
-    //  copy-button
-    //  call-button

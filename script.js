f7793baa36c7182abda8ce8d13f97b25fbb09849
Emrul Islam <emrul.mpl@gmail.com>
2025-08-27 01:02:40 +0600

    basic js class and id declare in js file as comments

---
+++ script.js
@@ -0,0 +1,44 @@
+//  Heart Icons//
+
+// Clicking on the 💗 heart icon of any card will increase the count in the Navbar
+
+//  Call Buttons//
+
+// On clicking a card's Call Button, following actions will happen:
+    // Show an alert with a message including the service name and number
+    // Each call will cut 20 coins. Reduce Coin after each click.
+    // If coins are less than 20, show a relevant alert and terminate the process.
+    // Add this service into the Call History section with:
+        // Service name
+        // Service number
+
+
+//  Call History Section//
+
+// Show all called services with name & number. This will empty initially. when call button clicked it will filled dynamically.
+// A Clear History button on the right
+// Clicking this button will remove all data from call history
+
+
+// Challenges Part (10 Marks)//
+
+    // On clicking the Copy button, show an alert and increase the copy count (3 Marks)
+
+    // Hotline number will be copied on click so it can be pasted anywhere (4 Marks)
+
+
+    // id
+
+    // heart-counter
+    // coin-counter
+    // copy-counter-button
+    
+    
+    // class names
+
+
+    // heart-button
+    //  service-name
+    //  service-number
+    //  copy-button
+    //  call-button
